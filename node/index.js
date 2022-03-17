bd45e6b493b6217adfdcdcd2bd40b1827018ded8
Craig Atallah Frost <c.frostwr1@gmail.com>
2022-03-17 22:38:39 +0000

    Add basic structure

---
+++ node/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Ably = require("ably");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/createTokenRequest", (req, res) => {
+  const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+  client.auth.createTokenRequest({ clientId: process.env.ABLY_CLIENT_ID }, (error, token) => {
+      res.status(200).json(token);
+    }
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
